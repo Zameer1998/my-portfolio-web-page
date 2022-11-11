10f7799e4337a952dcd9be4f050d1e6484851876
Zameer1998 <zameermohamed133@gmail.com>
2022-11-11 21:54:00 +0530

    my portfolio

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
-import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          i am zameer
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
