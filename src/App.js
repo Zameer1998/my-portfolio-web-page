711da6d8beb3a0a44efcce9602a08fcc63bb956d
Zameer1998 <mohamedajdal13@gmail.com>
2025-07-13 16:24:09 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
-      Zameer
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./navbar.css";
+import logo from "../assets/logo.jpg" // Adjust the path as necessary
+import contact from "../assets/contact.png"; // Adjust the path as necessary
+import { Link } from "react-scroll"; // Assuming you're using react-router for navigation
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <img src={logo} alt="logo" className="logo"/>
+            <div className="desktopMenu">
+                <Link className="desktopMenuListItem">Home</Link>
+                <Link className="desktopMenuListItem">About</Link>
+                <Link className="desktopMenuListItem">Portfolio</Link>
+                <Link className="desktopMenuListItem">Client</Link>
+            </div>
+            <button className="desktopMenuBtn">
+                <img src={contact} alt="" className="desktopMenuImg" />Contact Me</button>
+        </nav>
+    )
+}
+
+export default Navbar;
